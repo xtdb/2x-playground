68fb10df273dd23c8884df6aee359bed09b71360
Jeremy Taylor <jdt@juxt.pro>
2022-07-22 11:03:10 +0100

    playing with honeysql

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # core2-playground
+
+Add the following to your `~/.clojure/deps.edn`:
+
+```clojure
+:nREPL
+{:extra-deps
+  {nrepl/nrepl {:mvn/version "0.9.0"}}}
+```
+
+Run `clojure -X:deps prep :force true` to install deps.
+
+Run `clj -M:nREPL -m nrepl.cmdline` and connect from your editor.
--- src/xtexample.clj
+++ src/xtexample.clj
@@ -4,8 +4,11 @@
             [core2.sql.pgwire :as pgwire]
             [juxt.clojars-mirrors.nextjdbc.v1v2v674.next.jdbc :as jdbc]
             [juxt.clojars-mirrors.nextjdbc.v1v2v674.next.jdbc.connection :as jdbcc]
-            [juxt.clojars-mirrors.nextjdbc.v1v2v674.next.jdbc.result-set :as jdbcr])
-  (:import (java.sql Connection)))
+            [juxt.clojars-mirrors.nextjdbc.v1v2v674.next.jdbc.result-set :as jdbcr]
+            [jsonista.core :as json]
+            [honey.sql :as sql])
+  (:import (java.sql Connection)
+           (org.postgresql.util PGobject)))
 
 (declare node)
 (declare server)
@@ -23,17 +26,64 @@
   #_(read-line))
 
 (defn- jdbc-conn ^Connection [& params]
-  (jdbc/get-connection "jdbc:postgresql://localhost/xtdb"))
+  (jdbc/get-connection "jdbc:postgresql://localhost/xtdb/"))
+
+(def mapper (json/object-mapper {:decode-key-fn keyword}))
+(def ->json json/write-value-as-string)
+(def <-json #(json/read-value % mapper))
+
+(defn ->pgobject
+  "Transforms Clojure data to a PGobject that contains the data as
+  JSON. PGObject type defaults to `jsonb` but can be changed via
+  metadata key `:pgtype`"
+  [x]
+  (let [pgtype (or (:pgtype (meta x)) "jsonb")]
+    (doto (PGobject.)
+      (.setType pgtype)
+      (.setValue (->json x)))))
+
+(defn <-pgobject
+  "Transform PGobject containing `json` or `jsonb` value to Clojure
+  data."
+  [^org.postgresql.util.PGobject v]
+  (let [type  (.getType v)
+        value (.getValue v)]
+    (if (#{"jsonb" "json"} type)
+      (when value
+        (with-meta (<-json value) {:pgtype type}))
+      value)))
 
 (comment
-  (with-open [conn (jdbc-conn)]
-    (jdbc/execute! conn ["SELECT a.name FROM a"]))
-
   (def node (local-node/start-node {}))
 
   (def server (pgwire/serve node))
 
   (run {})
 
-  ;; SELECT foo.name FROM foo
+  (with-open [conn (jdbc-conn)]
+    (->> (jdbc/execute! conn (sql/format {:select :a.name :from :a}))
+         (map :name)
+         first))
+
+  )
+
+(comment
+  (def overlaps "APP_TIME OVERLAPS" :overlaps)
+
+  (sql/register-op! overlaps)
+
+  (defn- my-formatter [f x]
+    (let [[sql & params] (sql/format-expr x)]
+      (into [(str "FOR SYSTEM_TIME AS OF " sql )] params)))
+
+  (sql/register-clause! :for-system-time-as-of my-formatter :with-data)
+
+  (sql/clause-order)
+
+  (sql/format {:select [:foo.name :bar.also_name]
+               :from [:foo :bar]
+               :join-by [:left [[:baz]
+                                [:using :id]]]
+               :where [:overlaps :foo.APP_TIME :bar.APP_TIME]
+               :for-system-time-as-of [:inline 123]})
   )
